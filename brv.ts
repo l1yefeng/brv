096ea2aee80d3ed1a70c034ecdfa6b19a891ac98
LYF <dev@lyfeng.xyz>
2022-06-15 14:59:12 -0400

    highlight current point in toc

--- brv.ts
+++ brv.ts
@@ -1,11 +1,29 @@
 
-let tocOn = false
-
-document.addEventListener("keydown", keyListener)
-
-// space key : toggle toc
+interface TocPoint {
+    anchor: HTMLAnchorElement;
+    pos: number;
+}
 
 const TOC_ID = "brv-toc"
+const tocDiv = document.getElementById(TOC_ID)
+
+let tocOn = false
+let tocPoints = makeTocPoints()
+
+// respond to keys
+document.addEventListener("keydown", keyListener)
+
+// initial highlight current point in toc
+// respond to scrolling
+if (tocPoints.length > 0) {
+    if (tocPoints.length == 1) {
+        highlightToc(tocPoints[0].anchor)
+    } else {
+        const selected = findCurrentTocPoint()
+        highlightToc(selected)
+        document.addEventListener("scroll", throttle(scrollListener, 100))
+    }
+}
 
 function keyListener(event: KeyboardEvent) {
     switch (event.code) {
@@ -14,7 +32,7 @@ function keyListener(event: KeyboardEvent) {
 
             event.preventDefault()
             if (tocOn) {
-                document.getElementById(TOC_ID).style.display = "none"
+                tocDiv.style.display = "none"
                 tocOn = false
             }
             break
@@ -23,10 +41,95 @@ function keyListener(event: KeyboardEvent) {
 
             event.preventDefault()
 
-            const elem = document.getElementById(TOC_ID)
-            elem.style.display = tocOn ? "none" : "block"
+            tocDiv.style.display = tocOn ? "none" : "block"
             tocOn = !tocOn
 
             break
+    }
+}
+
+function scrollListener() {
+
+    // highlight
+    const selected = findCurrentTocPoint()
+    highlightToc(selected)
+
+    // location
+    const href = selected.href
+    const hashIdx = href.indexOf("#")
+    if (hashIdx >= 0) {
+        const id = href.substring(hashIdx+1)
+        const elem = document.getElementById(id)
+        if (elem != null) {
+            elem.removeAttribute("id")
+            window.location.hash = "#" + id
+            elem.id = id
+        }
+    }
+
+}
+
+function findCurrentTocPoint(): HTMLAnchorElement {
+
+    const mid = window.scrollY + window.innerHeight/2
+
+    let curr: number
+    for (curr = 0; curr < tocPoints.length; curr++) {
+        const tPos = tocPoints[curr].pos
+        if (tPos > mid) {
+            break
+        }
+    }
+    curr--
+
+    return tocPoints[curr < 0 ? 0 : curr].anchor
+}
+
+function highlightToc(a: HTMLAnchorElement) {
+    const className = "current"
+    tocPoints.forEach(({anchor}) => {
+        anchor.parentElement.classList.remove(className)
+    })
+    a.parentElement.classList.add(className)
+}
+
+// calculate the position of each toc target on the current page
+function makeTocPoints(): TocPoint[] {
+    const pageHref = window.location.pathname
+    const anchors = tocDiv.querySelectorAll(`a[href^="${pageHref}"]`)
+
+    let points: TocPoint[] = []
+    anchors.forEach((elem: HTMLAnchorElement) => {
+        let pos: number
+
+        const href = elem.href
+        const hashIdx = href.indexOf("#")
+        if (hashIdx < 0) { // base
+            pos = 0
+        } else {
+            const id = href.substring(hashIdx + 1)
+            const target = document.getElementById(id)
+            pos = target == null ? 0 : target.offsetTop
+        }
+
+        points.push({anchor: elem, pos})
+    })
+
+    return points
+}
+
+function throttle(fn: () => void, wait: number): () => void {
+    let waiting = false
+
+    return function() {
+        if (waiting) {
+            return
+        }
+
+        waiting = true
+        setTimeout(() => {
+            fn.apply(this)
+            waiting = false
+        }, wait)
     }
 }
